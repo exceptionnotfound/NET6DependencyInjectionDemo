8ed08c08112525434bfa333edbbedbc7cd7d8002
Matthew Jones <10541872+exceptionnotfound@users.noreply.github.com>
2021-12-15 10:33:33 -0700

    Added more comments to Program.cs

--- DependencyInjectionNET6Demo/Program.cs
+++ DependencyInjectionNET6Demo/Program.cs
@@ -2,7 +2,6 @@ using DependencyInjectionNET6Demo.Repositories;
 using DependencyInjectionNET6Demo.Repositories.Interfaces;
 using DependencyInjectionNET6Demo.Services;
 using DependencyInjectionNET6Demo.Services.Interfaces;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,9 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 //Transient services are created by the container every time they need to be injected.
 builder.Services.AddTransient<IMovieRepository, MovieRepository>();
-
-//Uncomment the below line
-builder.Services.AddTransient<IActorRepository, ActorRepository>();
+builder.Services.AddTransient<IActorRepository, ActorRepository>(); //In the ActorRepository.cs file, uncomment the class's code to cause a circular dependency
 
 //Scoped services are created once per request.
 //That means, if a dependency needs to be injected twice in the same request,
@@ -21,6 +18,8 @@ builder.Services.AddScoped<ICustomLogger, CustomLogger>();
 
 //Singleton services are only created once; all injections receive the same instance.
 builder.Services.AddSingleton<ICacheService, CacheService>();
+
+//.NET includes a bunch of helper methods which can inject commonly-used services, such as the ones for Razor Pages.
 builder.Services.AddRazorPages().AddRazorPagesOptions(options =>
 {
     options.Conventions.AddPageRoute("/Movies", "");
